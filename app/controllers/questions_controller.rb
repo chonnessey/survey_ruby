ef273be09d2eb46c41b24c4176bf6fa8d10b058e
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-03 15:33:51 -0700

    add routes to questions
    Co-authored-by: Michael Reiersgaard <michael.reiersgaard@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     @survey = Survey.find(params[:survey_id])
     @question = @survey.questions.new(question_params)
     if @question.save
-      redirect_to survey_path(@suvey)
+      redirect_to survey_path(@survey)
     else
       render :new
     end
@@ -29,7 +29,7 @@ class QuestionsController < ApplicationController
 
   def update
     @question = Question.find(params[:id])
-    if @question.update(questions_params)
+    if @question.update(question_params)
       redirect_to survey_path(@question.survey)
     else
       @survey = Survey.find(params[:survey_id])
@@ -40,7 +40,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
-    redirect_to survey_path(@question.suvey)
+    redirect_to survey_path(@question.survey)
   end
 
   private
